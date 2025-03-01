2ab1a4c5756ed1f1d9ed5500af865cd412a804c8
Gideon Felt <gideon.felt@transimpact.com>
2025-02-28 18:08:26 -0700

    Updates

---
+++ precache-manifest.680a14ef41060acceb8d498e1309e551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d36fbbe1782606998c27626ef687ecd",
+    "url": "/SkullKingScorer/index.html"
+  },
+  {
+    "revision": "6d048f7bf53b9182ea89",
+    "url": "/SkullKingScorer/static/css/main.8a8980a8.chunk.css"
+  },
+  {
+    "revision": "652203d99a0da0d96a0b",
+    "url": "/SkullKingScorer/static/js/2.2cecdcf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SkullKingScorer/static/js/2.2cecdcf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d048f7bf53b9182ea89",
+    "url": "/SkullKingScorer/static/js/main.b8fc2d43.chunk.js"
+  },
+  {
+    "revision": "7e47188904a242080209",
+    "url": "/SkullKingScorer/static/js/runtime-main.a7ab9d6e.js"
+  }
+]);
